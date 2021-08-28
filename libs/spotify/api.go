fbd0173a2357e95d070c8dcbe796d4a9974deb92
Tran Khanh <tran_khanh@outlook.com>
2021-08-28 10:04:04 +0700

    [I] get for api

--- libs/spotify/api.go
+++ libs/spotify/api.go
@@ -1,9 +1,13 @@
 package spotify
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"path"
 )
 
 const APIhost = "api.spotify"
@@ -32,4 +36,45 @@ func (a *API) delete(apiVersion, endpoint string, query url.Values) error {
 	return a.call(http.MethodDelete, apiVersion, endpoint, query, nil, nil)
 }
 
-func 
+func (a *API) call(method, apiVersion, endpoint string, query url.Values, body io.Reader, result interface{}) error {
+	url := url.URL{
+		Host:     APIHost,
+		Path:     path.Join(apiVersion, endpoint),
+		RawQuery: query.Encode(),
+		Scheme:   "https",
+	}
+
+	req, err := http.NewRequest(method, url.String(), body)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.token))
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	// Success
+	if res.StatusCode >= 200 && res.StatusCode < 300 {
+		if result != nil {
+			if err := json.NewDecoder(res.Body).Decode(result); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	// Error
+	spotifyErr := &struct {
+		Error Error `json:"error"`
+	}{}
+	if err := json.NewDecoder(res.Body).Decode(spotifyErr); err != nil {
+		return err
+	}
+
+	return errors.New(spotifyErr.Error.Message)
+}
